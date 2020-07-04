fa227515201023128541d17ced80db874cebdcff
Saif <saif.as7777@gmail.com>
2020-07-04 17:45:12 +0300

    add auth guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { RoomsComponent } from './components/rooms/rooms.component';
 import { LoginComponent } from './components/login/login.component';
 import { RoomComponent } from './components/room/room.component';
-
+import {
+  AuthGuard
+} from './auth.guard';
 
 const routes: Routes = [
   { path: 'rooms', component: RoomsComponent },
-  { path: 'room/:name', component: RoomComponent },
+  { path: 'room/:name', component: RoomComponent, canActivate: [AuthGuard] },
 
   { path: '', component: LoginComponent },
+  { path: '**', redirectTo: ''}
 ];
 
 @NgModule({
